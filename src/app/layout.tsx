94ca69f914b6ed9d4b1cc48c1d86730e136d6eee
Nivekdex <nivekpal@gmail.com>
2026-08-02 08:39:29 +0100

    feat: implement root layout with global providers, SEO metadata, and landing page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,11 +98,11 @@ export default function RootLayout({
       </head>
       <body className={`${fondamento.variable} ${outfit.variable} antialiased min-h-screen flex flex-col font-sans`}>
         <QueryProvider>
-          <div className="fixed top-0 left-0 right-0 z-50">
+          <div className="sticky top-0 left-0 right-0 z-50 flex flex-col">
             <AnnouncementBar />
             <Navbar />
           </div>
-          <main className="flex-grow pt-[100px]">
+          <main className="flex-grow flex flex-col">
             {children}
           </main>
           <Footer />
